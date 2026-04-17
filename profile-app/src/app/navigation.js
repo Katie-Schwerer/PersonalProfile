f6d1e35f5a339cc51afb6c046a5ed315830419e9
Katie Schwerer <89220941+Katie-Schwerer@users.noreply.github.com>
2026-04-17 01:13:38 +0000

    Started  the navibar and creatin gthe layout

---
+++ profile-app/src/app/navigation.js
@@ -0,0 +1,32 @@
+import Link from 'next/link';
+import './globals.css';
+
+export default function Layout({ children }) {
+    return (
+        <>
+            <nav>
+                <div className="title-side">
+                    <span className='square'></span>
+                    <p><b>Katie Schwerer</b> / Web Developer</p>
+                </div>
+                <div className="pages-side">
+                    <ul>
+                        <li>
+                            <Link href="/">About Me</Link>
+                        </li>
+                        <li>
+                            <Link href="/resume">Resume</Link>
+                        </li>
+                        <li>
+                            <Link href="/projects">Project</Link>
+                        </li>
+                        <li>
+                            <Link href="/contact">Contact Me</Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+            <main>{children}</main>
+        </>
+    );
+}
