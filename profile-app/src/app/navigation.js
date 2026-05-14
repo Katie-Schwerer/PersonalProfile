497d62cd10a104684c1ba5cce88fade5ef025db3
Katie Schwerer <89220941+Katie-Schwerer@users.noreply.github.com>
2026-05-14 00:25:07 +0000

    Add the navbar to the main page and add some styles. Need to design the navbar

--- profile-app/src/app/navigation.js
+++ profile-app/src/app/navigation.js
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import './globals.css';
+import './css/navigation.css'
 
 export default function Layout({ children }) {
     return (
--- profile-app/src/app/page.js
+++ profile-app/src/app/page.js
@@ -1,8 +1,10 @@
+import Layout from "./navigation";
 
 
 export default function Home() {
   return (
     <div>
+      <Layout></Layout>
     </div>
   );
 }
